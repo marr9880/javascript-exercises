274dbff4bbe7ab9717b97e331872b2f58a048b17
Matt <229481176+marr9880@users.noreply.github.com>
2025-11-20 21:38:06 -0600

    Complete addSum exercise

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,27 @@
-const sumAll = function() {
+const sumAll = function(...numbers) {
+    let lowestNumber = Math.min(...numbers);
+    let highestNumber = Math.max(...numbers);
 
+    let range = [];
+    for (let i = lowestNumber; i <= highestNumber; i++) {
+        range.push(i);
+    }
+
+    let sum = range.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+
+    if (lowestNumber >= 0 && 
+        highestNumber >=0 && 
+        Number.isInteger(lowestNumber) &&
+        Number.isInteger(highestNumber)) {
+            return sum;
+        } else {
+            return "ERROR";
+        }
 };
+
+
+
+
 
 // Do not edit below this line
 module.exports = sumAll;
