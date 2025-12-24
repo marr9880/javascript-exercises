c8b98df9a8ea2aa62942133ec7ecd29bf545675e
Matt <229481176+marr9880@users.noreply.github.com>
2025-12-24 12:40:04 -0600

    Complete palindromes exercise

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let lowercasedString = string.toLowerCase();
+    let cleaned = lowercasedString.replace(/[^a-z0-9]/g, ''); //removes any characters that aren't nums or letters
+    let splitStringArray = cleaned.split("");
+    let reversedStringArray = splitStringArray.toReversed();
+    let reversedString = reversedStringArray.join("");
 
+    if (cleaned === reversedString) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
