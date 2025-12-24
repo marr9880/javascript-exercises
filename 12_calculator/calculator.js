4266e69fe4b3c9b1dd10417b34fc27a0185df312
Matt <229481176+marr9880@users.noreply.github.com>
2025-12-24 09:29:46 -0600

    Complete calculator exercise

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,35 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+  return parseFloat(num1) + parseFloat(num2);
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return parseFloat(num1) - parseFloat(num2);
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  return array.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((accumulator, currentValue) => accumulator * currentValue, 1)
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	return parseFloat(num1) ** parseFloat(num2)
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	if (n < 0) {
+    return "Factorial is not defined for negative numbers.";
+  } else if (n === 0 || n === 1) {
+    return 1;
+  } else {
+    let result = 1;
+    for (let i = 2; i <= n; i++) {
+      result *= i;
+    }
+    return result;
+  }
 };
 
 // Do not edit below this line
