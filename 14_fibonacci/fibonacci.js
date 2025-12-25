bf5479fae5420378598a13e1d184b9a9f0fa4ce0
Matt <229481176+marr9880@users.noreply.github.com>
2025-12-25 14:56:33 -0600

    Complete fibonacci exercise

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,6 +1,30 @@
-const fibonacci = function() {
+const fibonacciSequence = function(n) {
+    let fib = [1, 1];
 
+    if (n <=2) {
+        return fib.slice(0, n);
+    }
+
+    for (let i = 2; i < n; i++) {
+        fib.push(fib[i - 1] + fib[i - 2]);
+    }
+
+    return fib;
+    };
+
+const fibNum = fibonacciSequence(30);
+
+const fibonacci = function(n) {
+    if (parseInt(n) < 0) {
+        return "OOPS";
+    } else if (parseInt(n) === 0) {
+        return 0;
+    } else {
+        return fibNum[parseInt(n) - 1];
+    }
 };
+
+fibonacci(4);
 
 // Do not edit below this line
 module.exports = fibonacci;
