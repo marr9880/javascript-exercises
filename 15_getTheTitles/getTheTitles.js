fb6b53f1a43d34801fca443df1184ca5213b7a87
Matt <229481176+marr9880@users.noreply.github.com>
2025-12-26 18:04:34 -0600

    Complete getTheTitles exercise

--- 15_getTheTitles/getTheTitles.js
+++ 15_getTheTitles/getTheTitles.js
@@ -1,5 +1,16 @@
 const getTheTitles = function() {
-
+    const books = [
+      {
+        title: 'Book',
+        author: 'Name'
+      },
+      {
+        title: 'Book2',
+        author: 'Name2'
+      }
+    ]
+    const titles = books.map(book => book.title);
+    return titles;
 };
 
 // Do not edit below this line
