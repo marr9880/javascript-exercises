c9112e00bf7653a0571fa42b9df918dc3c6e7a95
Matt <229481176+marr9880@users.noreply.github.com>
2025-12-26 19:07:06 -0600

    Complete findTheOldest exercise

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,28 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const currentYear = new Date().getFullYear();
+    function calculateAge(person) {
+        if (person.yearOfBirth && person.yearOfDeath) {
+            return person.yearOfDeath - person.yearOfBirth;
+        } else {
+            return currentYear - person.yearOfBirth;
+        }
+    }
+    const peopleWithAge = people.map(person => {
+        const age = calculateAge(person);
+        return {
+            ...person,
+            currentAge: age
+        };
+    });
 
+    function getOldestPerson(peopleWithAge) {
+        return peopleWithAge.reduce((oldestPerson, currentPerson) => {
+            return (currentPerson.currentAge > oldestPerson.currentAge) ? currentPerson : oldestPerson;
+        });
+    }
+    const oldest = getOldestPerson(peopleWithAge);
+
+    return oldest;
 };
 
 // Do not edit below this line
